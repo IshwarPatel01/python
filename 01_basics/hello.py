d8a5ca0c263ba82d70d557f8bcf1e0995315c2b3
Ishwar Patel <ishwarrpatel50@gmail.com>
2024-10-06 13:23:59 +0530

    add learn about python shell

--- 01_basics/hello.py
+++ 01_basics/hello.py
@@ -8,3 +8,10 @@ num(4)
 # yeh jo method likha hai woh i.py file me print karna hain 
 # toh basically humne import kia function name num ko aur print kia i.py file se 
 #  basically call kia | yeh hua ki hello.py file ke function ko humne i.py mei import kia fuction ko aur use kia simple and easy
+
+
+
+#python shell import used variable
+n1 = 2
+n2 = 3
+n3 = 4
