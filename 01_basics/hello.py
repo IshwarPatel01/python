64117c5708dfa883cdfcffdb8f87170a29c281aa
Ishwar Patel <ishwarrpatel50@gmail.com>
2024-10-06 11:21:55 +0530

    add python basic with internal working

---
+++ 01_basics/hello.py
@@ -0,0 +1,10 @@
+print("hello python")
+
+def num(n):
+    print(n)
+
+num(4)
+
+# yeh jo method likha hai woh i.py file me print karna hain 
+# toh basically humne import kia function name num ko aur print kia i.py file se 
+#  basically call kia | yeh hua ki hello.py file ke function ko humne i.py mei import kia fuction ko aur use kia simple and easy
---
+++ 01_basics/i.py
@@ -0,0 +1,3 @@
+from hello import num
+
+num(33)
