6ed6bfc533ad17ff4b68fb0b3469804b28060dd9
Ishwar Patel <ishwarrpatel50@gmail.com>
2024-10-08 17:07:55 +0530

    add learn about scope and closure

---
+++ 06_scopes/01_scopes.py
@@ -0,0 +1,59 @@
+#scopes ko namespace bhi kaha jata hain
+
+#konsa variable kaha mere liye available hai kaha nai yeh pata karege
+
+#importance sabse jyada global scope aur function scope ko di jati hain
+
+#hum sabse jyada concern rahete function ke scope ke liye qki ifelse mei hum sirf condition check karte hain.
+
+#jab bhi aap ek function banayege toh aise samjiye ki aapne memory ke andr 1ghr banaya hain 
+
+#humare function ke andr jo bhi variable ya jo bhi declare karte hain woh sabka dena dena bas function ke andr hii hai
+
+
+#aisa bhi ho sakta hai ki woh ghr mei aap chote chote rooms banaye , unko maine scope access kar sakta hai , main ghr ek hi hai toh function toh ek hii mar bana
+
+# jo room ke andr a banaya woh ghr ko access rahega?
+
+
+#yeh  joh file hai global hain 
+
+
+username = "ishwar"   #gloabal variable
+
+def func():
+    # username = "chai"   #main scope variable
+    print(username)
+
+print(username)
+func()
+
+
+x = 99
+
+# def func2(y):
+#     z = x + y
+#     return z 
+
+# result = func2(1)
+# print(result)
+
+
+# def func3():
+#     global x  #avoid this
+#     x = 88
+
+# func3()
+# # print(func3())
+# print(x)
+
+def f1():
+    # x = 88
+    def f2():
+        print(x)
+    return f2
+
+myresult = f1()
+myresult()
+
+#closure start from 20mins # bag throry also called 
